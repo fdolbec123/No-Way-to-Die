9cc96212bde1034af002b80736a092b7dac77958
fdolbec123 <infos.fpmedia@gmail.com>
2025-09-04 00:11:01 -0400

    Localization can now be changed in menu

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -1,8 +1,14 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+using TMPro;
+using UnityEngine.Localization.Settings;
+using UnityEditor.Localization.Editor;
 
 public class MainMenuScript : MonoBehaviour
 {
+    //vars:
+    public TMP_Dropdown languageMenu;
     //Script for play button:
     public void PlayGame()
     {
@@ -11,6 +17,14 @@ public class MainMenuScript : MonoBehaviour
     }
 
     //Script for the options panel:
+    public void SaveSettings()
+    {
+        Debug.Log(languageMenu.value);
+        //languageMenu.value is an Int. If the locale table is in the same order as the dropdown menu, the Int value will be matching, so we can use it.
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[languageMenu.value];
+        Debug.Log("Saving...");
+        PlayerPrefs.Save();
+    }
 
 
     //Script that close the game:
